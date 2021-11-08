db63080c0683baaf35d3d6c615e22a82e195f29c
MayPhoo-GitHub <mayphoowai14@gmail.com>
2021-11-08 10:17:37 +0630

    [migration]added migration

---
+++ group3/database/migrations/2021_11_07_162037_create_reservation_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('room_id')->references('id')->on('rooms');
+            $table->char('customer_name',255);
+            $table->integer('phone');
+            $table->char('hotel_name',255);
+            $table->char('room_number',255);
+            $table->integer('number_of_guest');
+            $table->date('check_in');
+            $table->date('check_out');
+            $table->boolean('status');
+            $table->timestamps();
+            $table->softDeletes('deleted_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservation');
+    }
+}
