47a882282ecbdc54f9aa20d9a74cc0adcab16241
MayPhoo-GitHub <mayphoowai14@gmail.com>
2021-11-08 21:26:42 +0630

    [develop]added modified migration

---
+++ group3/database/migrations/2021_11_07_154950_create_hotel_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Location;
+
+class CreateHotelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->id();
+            $table->char('hotel_name',255);
+            $table->char('location',255);
+            $table->char('description',255);
+            $table->char('phone',20);
+            $table->timestamps();
+            $table->softDeletes('deleted_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotel');
+    }
+}
