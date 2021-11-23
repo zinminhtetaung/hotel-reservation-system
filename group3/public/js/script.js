e25e1e4a0191572185f03e8250b2389fe1384c5a
unknown <kaan.miit@gmail.com>
2021-11-23 10:59:08 +0630

    [feature/RoomCRUD] fixed review

--- group3/public/js/script.js
+++ group3/public/js/script.js
@@ -40,19 +40,3 @@ if(copyTextareaBtn){
         this.classList.add("active");
     }
 }
-showMenu('header-toggle','navbar')
-
-/*==================== LINK ACTIVE ====================*/
-const linkColor = document.querySelectorAll('.nav__link')
-
-<<<<<<< HEAD
-function colorLink(){
-    linkColor.forEach(l => l.classList.remove('active'))
-    this.classList.add('active')
-}
-
-linkColor.forEach(l => l.addEventListener('click', colorLink))
-=======
-    linkColor.forEach((l) => l.addEventListener("click", colorLink));
-}
->>>>>>> origin/develop
