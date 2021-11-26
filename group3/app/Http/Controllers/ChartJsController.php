957dc4069223078eb0de54cd900837b6d7563808
MayPhoo-GitHub <mayphoowai14@gmail.com>
2021-11-26 08:50:20 +0630

    [feature/codeReviewMP]fix code review

--- group3/app/Http/Controllers/ChartJsController.php
+++ group3/app/Http/Controllers/ChartJsController.php
@@ -13,7 +13,7 @@ class ChartJsController extends Controller
             $months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
 
             $reservations = [];
-            foreach ($months as $key => $value) {
+            foreach ($months as $value) {
                 $reservations[] = Reservation::where( DB::raw("DATE_FORMAT(check_in, '%m')"),$value)
                 ->withTrashed()
                 ->count();
